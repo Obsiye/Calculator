71a982d280f0241c20fca7010c39b5f444b0a92b
Obsiye <mrabdirizak@gmail.com>
2018-10-31 11:14:06 +0000

    Test that two numbers can be multiplied

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,6 +3,10 @@ require 'calculator'
 describe Calculator do
 
     it 'can #multiply' do
-      expect(Calculator.new).to respond_to(:multiply)
+      expect(subject).to respond_to(:multiply)
+    end
+
+    it 'can #multiply two numbers' do
+      expect(subject.multiply(1,2)).to eq 2
     end
 end
